a6bce760affaa4b826d6e65b1020f498825da86a
John Burke <burkejr19@gcc.edu>
2021-03-24 16:28:01 -0400

    added description and main methods

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,12 @@
-
+/** Author:  John Burke, Sydnee Charles
+  * Course:  COMP 342 Data Communications and Networking
+  * Date:    12 April 2021
+  * Description: 
+*/
 public class Client {
 
+	public static void main(String[] args) {
+		
+	}
+	
 }
